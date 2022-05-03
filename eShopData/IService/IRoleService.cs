e110ab4d4f1761616eff3d40ad304c3ec18677cd
meo9xit <meo9xit@gmail.com>
2022-05-03 22:34:34 +0700

    update sldfk

--- eShopData/IService/IRoleService.cs
+++ eShopData/IService/IRoleService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eShopData.DTOs.Role;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace eShopData.IService
 {
     public interface IRoleService
     {
+        Task<RoleModel> AddRoleAsync(RoleModel roleModel);
+        Task<RoleModel> UpdateRoleAsync(RoleModel roleModel);
+        Task DeleteRoleAsync(string roleId);
     }
 }
---
+++ eShopData/Services/RoleService.cs
@@ -0,0 +1,28 @@
+﻿using eShopData.DTOs.Role;
+using eShopData.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopData.Services
+{
+    public class RoleService : IRoleService
+    {
+        public Task<RoleModel> AddRoleAsync(RoleModel roleModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRoleAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoleModel> UpdateRoleAsync(RoleModel roleModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
